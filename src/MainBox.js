c04807c1095933a5c5e62ab1c79c2ca5ff5dbebf
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2020-12-02 10:57:10 -0500

    Fixed useEffect() issues

--- src/MainBox.js
+++ src/MainBox.js
@@ -6,6 +6,116 @@ import acoustic from './arrays/acoustic'
 import chaosEngine from './arrays/chaos-engine'
 
 // moved state & playSample up so both LP & RP have access via props
+function MainBox() {
+  const [currentSample, setCurrentSample] = useState('')
+  const [soundBank, setSoundBank] = useState(synthwave)
+  const [bankName, setBankName] = useState('Synthwave')
+  const [power, setPower] = useState(true)
+  const [volume, setVolume] = useState(1)
+
+  /* useEffect() is a hook that can replace lifecycle methods such as componentDidMount() & componentWillUnmount() */
+  useEffect(() => {
+    // add event listener for drumpad keypresses
+    document.addEventListener('keydown', handleKeyPress)
+    // add event listener for removing css animation class
+    document.addEventListener('animationend', removeAnimClass)
+
+    // remove event Listeners
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress)
+      document.removeEventListener('animationend', removeAnimClass)
+    }
+  })
+
+  function playSample(elementId) {
+    const element = document.getElementById(elementId)
+
+    // check if power is on first!
+    if (power) {
+      // add class to trigger css animation
+      element.classList.add('pad-anim')
+      element.firstChild.volume = volume
+
+      // need to use play() method on nested <audio> element
+      document.getElementById(elementId).firstChild.play()
+      setCurrentSample(elementId)
+    }
+  }
+
+  function handleClick(event) {
+    playSample(event.target.id)
+  }
+
+  function handleKeyPress(event) {
+    // use keycode to to get sample name
+    const elementId = soundBank.filter(entry => entry.keycode === event.keyCode)
+
+    if (elementId[0]) playSample(elementId[0].name)
+  }
+
+  function removeAnimClass(event) {
+    document.getElementById(event.target.id).classList.remove('pad-anim')
+  }
+
+  function handleBankBtnClick(event) {
+    // let { soundBank, bankName } = this.state
+
+    if (event.target.id === 'bank1') {
+      setSoundBank(synthwave)
+      setBankName('Synthwave')
+    } else if (event.target.id === 'bank2') {
+      setSoundBank(acoustic)
+      setBankName('Acoustic')
+    } else if (event.target.id === 'bank3') {
+      setSoundBank(chaosEngine)
+      setBankName('Chaos Engine')
+    }
+  }
+
+  function handlePowerBtn() {
+    // flip power state boolean
+    setPower(prevState => !prevState)
+    setCurrentSample('')
+  }
+
+  function handleVolBtn(event) {
+    // find out which vol btn was pressed
+    const whichBtn = event.target.id
+
+    setVolume(prevState => {
+      if (whichBtn === 'vol-up' && prevState < 1) {
+        return Number((prevState + 0.1).toFixed(1))
+      } else if (whichBtn === 'vol-down' && prevState >= 0.1) {
+        return Number((prevState - 0.1).toFixed(1))
+      } else {
+        return prevState
+      }
+      /* because of floating point precision errors we need to round the return val to 1 decimal place; tofixed() returns a string, so we convert back to a num using Number() */
+    })
+  }
+
+  return (
+    <div className="main-box">
+      <LeftPanel
+        display={currentSample}
+        handleBankBtnClick={handleBankBtnClick}
+        bankName={bankName}
+        handlePowerBtn={handlePowerBtn}
+        power={power}
+        volume={volume}
+        handleVolBtn={handleVolBtn}
+      />
+      <RightPanel
+        handleClick={handleClick}
+        soundBank={soundBank}
+        power={power}
+      />
+    </div>
+  )
+}
+
+// OLD CLASS-BASED VERSION of MainBox
+
 // class MainBox extends React.Component {
 //   constructor() {
 //     super()
@@ -133,105 +243,5 @@ import chaosEngine from './arrays/chaos-engine'
 //     )
 //   }
 // }
-
-function MainBox() {
-  const [currentSample, setCurrentSample] = useState('')
-  const [soundBank, setSoundBank] = useState(synthwave)
-  const [bankName, setBankName] = useState('Synthwave')
-  const [power, setPower] = useState(true)
-  const [volume, setVolume] = useState(1)
-
-  /* useEffect() is a hook that can replace lifecycle methods; to run only once like componentDidMount, we supply an empty array as the 2nd arg*/
-  useEffect(() => {
-    // add event listener for drumpad keypresses
-    document.addEventListener('keydown', handleKeyPress)
-    // add event listener for removing css animation class
-    document.addEventListener('animationend', event =>
-      document.getElementById(event.target.id).classList.remove('pad-anim')
-    )
-  })
-
-  function playSample(elementId) {
-    const element = document.getElementById(elementId)
-
-    // check if power is on first!
-    if (power) {
-      // add class to trigger css animation
-      element.classList.add('pad-anim')
-      element.firstChild.volume = volume
-
-      // need to use play() method on nested <audio> element
-      document.getElementById(elementId).firstChild.play()
-      setCurrentSample(elementId)
-    }
-  }
-
-  function handleClick(event) {
-    playSample(event.target.id)
-  }
-
-  function handleKeyPress(event) {
-    // use keycode to to get sample name
-    const elementId = soundBank.filter(entry => entry.keycode === event.keyCode)
-
-    if (elementId[0]) playSample(elementId[0].name)
-  }
-
-  function handleBankBtnClick(event) {
-    // let { soundBank, bankName } = this.state
-
-    if (event.target.id === 'bank1') {
-      setSoundBank(synthwave)
-      setBankName('Synthwave')
-    } else if (event.target.id === 'bank2') {
-      setSoundBank(acoustic)
-      setBankName('Acoustic')
-    } else if (event.target.id === 'bank3') {
-      setSoundBank(chaosEngine)
-      setBankName('Chaos Engine')
-    }
-  }
-
-  function handlePowerBtn() {
-    // flip power state boolean
-    setPower(prevState => !prevState)
-    setCurrentSample('')
-  }
-
-  function handleVolBtn(event) {
-    // find out which vol btn was pressed
-    const whichBtn = event.target.id
-
-    setVolume(prevState => {
-      if (whichBtn === 'vol-up' && prevState < 1) {
-        return Number((prevState + 0.1).toFixed(1))
-      } else if (whichBtn === 'vol-down' && prevState >= 0.1) {
-        return Number((prevState - 0.1).toFixed(1))
-      } else {
-        return prevState
-      }
-      /* because of floating point precision errors we need to round the return val to 1 decimal place; tofixed() returns a string, so we convert back to a num using Number() */
-    })
-  }
-
-  return (
-    <div className="main-box">
-      <LeftPanel
-        display={currentSample}
-        handleBankBtnClick={handleBankBtnClick}
-        bankName={bankName}
-        handlePowerBtn={handlePowerBtn}
-        power={power}
-        volume={volume}
-        handleVolBtn={handleVolBtn}
-      />
-      <RightPanel
-        handleClick={handleClick}
-        soundBank={soundBank}
-        power={power}
-      />
-    </div>
-  )
-}
 
 export default MainBox
