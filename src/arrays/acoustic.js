3a321092feda2a756e383c693b29d8d60d342ee0
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2020-12-10 15:27:52 -0500

    acoustic to local files

--- src/arrays/acoustic.js
+++ src/arrays/acoustic.js
@@ -1,75 +1,76 @@
+import kick1 from '../samples/acoustic/kick1.mp3'
+import kick2 from '../samples/acoustic/kick2.mp3'
+import snare1 from '../samples/acoustic/snare1.mp3'
+import snare2 from '../samples/acoustic/snare2.mp3'
+import tom1 from '../samples/acoustic/tom1.mp3'
+import tom2 from '../samples/acoustic/tom2.mp3'
+import hihatClosed from '../samples/acoustic/hihatClosed.mp3'
+import hihatOpen from '../samples/acoustic/hihatOpen.mp3'
+import cymbalCrash from '../samples/acoustic/cymbalCrash.mp3'
+
 const acoustic = [
   {
     trigger: 'Q',
     keycode: 81,
     pad: 'padQ',
     name: 'Kick-1',
-    src:
-      'https://raw.githubusercontent.com/caffeinated-pixels/drum-samples/main/acoustic/Kick-1.mp3'
+    src: kick1
   },
   {
     trigger: 'W',
     keycode: 87,
     pad: 'padW',
     name: 'Kick-2',
-    src:
-      'https://raw.githubusercontent.com/caffeinated-pixels/drum-samples/main/acoustic/Kick-2.mp3'
+    src: kick2
   },
   {
     trigger: 'E',
     keycode: 69,
     pad: 'padE',
     name: 'Snare-1',
-    src:
-      'https://raw.githubusercontent.com/caffeinated-pixels/drum-samples/main/acoustic/Snare-1.mp3'
+    src: snare1
   },
   {
     trigger: 'A',
     keycode: 65,
     pad: 'padA',
     name: 'Snare-2',
-    src:
-      'https://raw.githubusercontent.com/caffeinated-pixels/drum-samples/main/acoustic/Snare-2.mp3'
+    src: snare2
   },
   {
     trigger: 'S',
     keycode: 83,
     pad: 'padS',
     name: 'Tom-1',
-    src:
-      'https://raw.githubusercontent.com/caffeinated-pixels/drum-samples/main/acoustic/Tom-1.mp3'
+    src: tom1
   },
   {
     trigger: 'D',
     keycode: 68,
     pad: 'padD',
     name: 'Tom-2',
-    src:
-      'https://raw.githubusercontent.com/caffeinated-pixels/drum-samples/main/acoustic/Tom-2.mp3'
+    src: tom2
   },
   {
     trigger: 'Z',
     keycode: 90,
     pad: 'padZ',
     name: 'Hihat-closed',
-    src:
-      'https://raw.githubusercontent.com/caffeinated-pixels/drum-samples/main/acoustic/Hihat-closed.mp3'
+    src: hihatClosed
   },
   {
     trigger: 'X',
     keycode: 88,
     pad: 'padX',
     name: 'Hihat-open',
-    src:
-      'https://raw.githubusercontent.com/caffeinated-pixels/drum-samples/main/acoustic/Hihat-open.mp3'
+    src: hihatOpen
   },
   {
     trigger: 'C',
     keycode: 67,
     pad: 'padC',
     name: 'Cymbal-crash',
-    src:
-      'https://raw.githubusercontent.com/caffeinated-pixels/drum-samples/main/acoustic/Cymbal-crash.mp3'
+    src: cymbalCrash
   }
 ]
 
