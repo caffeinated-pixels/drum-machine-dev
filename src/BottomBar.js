232af9c02253a5df28ba8d6077f68be4271ac712
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-01-18 11:20:22 -0500

    added title attr to link

--- src/BottomBar.js
+++ src/BottomBar.js
@@ -5,7 +5,12 @@ export default function BottomBar() {
     <div className="bottom-bar text-center">
       <p>
         Coded & abused by&nbsp;
-        <a href="https://steviegill-webportfolio.netlify.app/">Stevie Gill</a>
+        <a
+          href="https://steviegill-webportfolio.netlify.app/"
+          title="Stevie's web app portofolio page"
+        >
+          Stevie Gill
+        </a>
       </p>
     </div>
   )
