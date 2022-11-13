c06128e6a13e7abdfe24ccde5f2d938ec9d8afa5
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-11-13 07:46:27 -0500

    fix: wrong app for test name

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import renderer from 'react-test-renderer'
 import { App } from '../App'
 
-describe('Calculator app', () => {
+describe('Drum machine app', () => {
   it('Matches DOM tree snapshot', () => {
     const domTree = renderer.create(<App />).toJSON()
     expect(domTree).toMatchSnapshot()
